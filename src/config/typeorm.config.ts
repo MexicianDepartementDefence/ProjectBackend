8e70a4a0df2fb2be8f2f28abcad84cbf2730a557
MexicianDepartementDefense <faisal216093@gmail.com>
2023-11-10 10:24:27 +0700

    Bekerja

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,8 +2,8 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: "mysql",
   host: "localhost",
-  port: 3306, //port default 3306 lihat xampp
-  username: "root", // username default xampp root
+  port: 3306, // Number(process.env.DB_PORT), //port default 3306 lihat xampp
+  username: "root", // process.env.DB_USERNAME, // username default xampp root
   password: "", // password default xampp string kosong
   database: "isengbet",
   entities: ["dist/**/*.entity{.ts,.js}"],
